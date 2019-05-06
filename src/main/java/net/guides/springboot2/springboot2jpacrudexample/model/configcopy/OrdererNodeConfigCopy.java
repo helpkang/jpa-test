c4ba6904f99a18b31f8157730eb0bbb68e9e9238
helpkang <helpkang@gmail.com>
2019-05-06 17:50:51 +0900

    entity annotation add

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/configcopy/OrdererNodeConfigCopy.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/configcopy/OrdererNodeConfigCopy.java
@@ -1,6 +1,9 @@
 package net.guides.springboot2.springboot2jpacrudexample.model.configcopy;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
 @DiscriminatorValue("orderer")
 public class OrdererNodeConfigCopy extends NodeConfigCopy{
 
--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/configcopy/PeerNodeConfigCopy.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/configcopy/PeerNodeConfigCopy.java
@@ -1,7 +1,9 @@
 package net.guides.springboot2.springboot2jpacrudexample.model.configcopy;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 
+@Entity
 @DiscriminatorValue("peer")
 public class PeerNodeConfigCopy extends NodeConfigCopy{
 	private static final long serialVersionUID = 1L;
